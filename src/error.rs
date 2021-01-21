757c52794e6484e0f8737b775a002debc916f97e
Alexandre Pauwels <8890201+ajpauwels@users.noreply.github.com>
2021-01-21 01:22:18 +0000

    feat: remove thiserror dependency
    Although thiserror provides an easy way to define custom error types via
    a proc macro, it greatly increases compile times when writing the error
    implementation by hand is quite easy.

--- src/error.rs
+++ src/error.rs
@@ -1,17 +1,47 @@
-use thiserror::Error;
+use std::error::Error;
+use std::fmt::Display;
 use warp::reject::Reject;
 
 /// Error type that converts to a warp::Rejection
-#[derive(Error, Debug)]
+#[derive(Debug)]
 pub enum SessionError {
-    #[error("Failed to load session")]
+    /// Represents an error which occurred while loading a session from
+    /// the backing session store.
     LoadError { source: async_session::Error },
 
-    #[error("Failed to store session")]
+    /// Represents an error that occurred while saving a session to
+    /// the backing session store.
     StoreError { source: async_session::Error },
 
-    #[error("Failed to destroy session")]
+    /// Represents an error that occurred while destroying a session
+    /// record from the backing session store.
     DestroyError { source: async_session::Error },
+}
+
+impl Error for SessionError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match *self {
+            SessionError::LoadError { ref source } => Some(source.as_ref()),
+            SessionError::StoreError { ref source } => Some(source.as_ref()),
+            SessionError::DestroyError { ref source } => Some(source.as_ref()),
+        }
+    }
+}
+
+impl Display for SessionError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match *self {
+            SessionError::LoadError { .. } => {
+                write!(f, "Failed to load session")
+            }
+            SessionError::StoreError { .. } => {
+                write!(f, "Failed to store session")
+            }
+            SessionError::DestroyError { .. } => {
+                write!(f, "Failed to destroy session")
+            }
+        }
+    }
 }
 
 impl Reject for SessionError {}
